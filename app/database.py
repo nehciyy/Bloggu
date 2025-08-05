e29037e45fa38ea64fa19a6df7fd8d67811d14ff
nehciyy <ycchiang001@mymail.sim.edu.sg>
2025-08-05 17:33:19 +0800

    chore: upgrade engine database size to be able to hold more data

--- app/database.py
+++ app/database.py
@@ -8,7 +8,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL,
+                        pool_size=20,
+                        max_overflow=30,
+                        pool_timeout=30,
+                    )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
